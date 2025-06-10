7a90d2385625b403bd4a3ad49c18a02b41e0cc30
sanjeev1991 <sanjeevsingh.php@gmail.com>
2025-06-10 11:27:03 +0530

    cors allowed

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*', // Allow all origins
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
