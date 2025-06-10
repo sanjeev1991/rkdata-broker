8bfa24bfa6331bc77367ae19cae5bf907df73905
sanjeev1991 <sanjeevsingh.php@gmail.com>
2025-06-10 14:42:40 +0530

    webhook changes

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -1,16 +1,24 @@
-import { Body, Controller, Headers, Post } from '@nestjs/common';
+import { All, Body, Controller, Headers, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { WebhookService } from './webhook.service';
 
 @Controller('webhook')
 export class WebhookController {
   constructor(private readonly webhookService: WebhookService) {}
 
-  @Post('upstox')
+  @All('upstox')
   async handleUpstoxWebhook(
+    @Req() req: Request,
     @Body() body: Record<string, unknown>,
     @Headers() headers: Record<string, string>,
   ) {
+    console.log('🔔 Webhook Triggered');
+    console.log('👉 Method:', req.method);
+    console.log('👉 Headers:', headers);
+    console.log('👉 Body:', body);
+
     const data = {
+      method: req.method,
       headers,
       body,
     };
